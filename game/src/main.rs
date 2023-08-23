7c469cbf504c06f0ad9f62c863e14d00445ad5a9
Luqcid71 <111896310+Luqcid71@users.noreply.github.com>
2023-08-23 00:30:33 -0400

    asd

--- game/src/main.rs
+++ game/src/main.rs
@@ -8,4 +8,9 @@ fn main() {
     println!("Hello, world");
     println!("amog");
     let my_dad = fatherstate::Gone;
+
+    match my_dad  {
+        fatherstate::Gone => println!("no milk?"),
+        _=>()
+    }
 }
