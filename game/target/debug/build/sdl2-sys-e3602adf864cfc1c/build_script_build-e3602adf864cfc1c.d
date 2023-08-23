8b3329db1f815f61df4c09c058260e2844d210fd
Wind292 <jp3@jayps.net>
2023-08-23 14:46:37 -0400

    idkman

---
+++ game/target/debug/build/sdl2-sys-e3602adf864cfc1c/build_script_build-e3602adf864cfc1c.d
@@ -0,0 +1,5 @@
+C:\Users\jp3\OneDrive\Documents\GitHub\Rust_Game\game\target\debug\build\sdl2-sys-e3602adf864cfc1c\build_script_build-e3602adf864cfc1c.exe: C:\Users\jp3\.cargo\registry\src\index.crates.io-6f17d22bba15001f\sdl2-sys-0.35.2\build.rs
+
+C:\Users\jp3\OneDrive\Documents\GitHub\Rust_Game\game\target\debug\build\sdl2-sys-e3602adf864cfc1c\build_script_build-e3602adf864cfc1c.d: C:\Users\jp3\.cargo\registry\src\index.crates.io-6f17d22bba15001f\sdl2-sys-0.35.2\build.rs
+
+C:\Users\jp3\.cargo\registry\src\index.crates.io-6f17d22bba15001f\sdl2-sys-0.35.2\build.rs:
