250ff6aaa2fb2edd4bdbae5704f03d46a09ea6e5
Luqcid71 <111896310+Luqcid71@users.noreply.github.com>
2023-08-23 16:52:38 -0400

    scuffed collision

--- game/target/debug/deps/game.d
+++ game/target/debug/deps/game.d
@@ -1,5 +1,5 @@
-C:\Users\jp3\OneDrive\Documents\GitHub\Rust_Game\game\target\debug\deps\game.exe: src\main.rs
+C:\Users\benja\OneDrive\Documents\GitHub\Rust_Game\game\target\debug\deps\game.exe: src\main.rs
 
-C:\Users\jp3\OneDrive\Documents\GitHub\Rust_Game\game\target\debug\deps\game.d: src\main.rs
+C:\Users\benja\OneDrive\Documents\GitHub\Rust_Game\game\target\debug\deps\game.d: src\main.rs
 
 src\main.rs:
--- game/target/debug/deps/sdl2_sys-9a5647e41400cad0.d
+++ game/target/debug/deps/sdl2_sys-9a5647e41400cad0.d
@@ -1,10 +1,10 @@
-C:\Users\jp3\OneDrive\Documents\GitHub\Rust_Game\game\target\debug\deps\sdl2_sys-9a5647e41400cad0.rmeta: C:\Users\jp3\.cargo\registry\src\index.crates.io-6f17d22bba15001f\sdl2-sys-0.35.2\src/lib.rs C:\Users\jp3\OneDrive\Documents\GitHub\Rust_Game\game\target\debug\build\sdl2-sys-480ea9db94c65671\out/sdl_bindings.rs
+C:\Users\benja\OneDrive\Documents\GitHub\Rust_Game\game\target\debug\deps\sdl2_sys-9a5647e41400cad0.rmeta: C:\Users\benja\.cargo\registry\src\index.crates.io-6f17d22bba15001f\sdl2-sys-0.35.2\src/lib.rs C:\Users\benja\OneDrive\Documents\GitHub\Rust_Game\game\target\debug\build\sdl2-sys-480ea9db94c65671\out/sdl_bindings.rs
 
-C:\Users\jp3\OneDrive\Documents\GitHub\Rust_Game\game\target\debug\deps\libsdl2_sys-9a5647e41400cad0.rlib: C:\Users\jp3\.cargo\registry\src\index.crates.io-6f17d22bba15001f\sdl2-sys-0.35.2\src/lib.rs C:\Users\jp3\OneDrive\Documents\GitHub\Rust_Game\game\target\debug\build\sdl2-sys-480ea9db94c65671\out/sdl_bindings.rs
+C:\Users\benja\OneDrive\Documents\GitHub\Rust_Game\game\target\debug\deps\libsdl2_sys-9a5647e41400cad0.rlib: C:\Users\benja\.cargo\registry\src\index.crates.io-6f17d22bba15001f\sdl2-sys-0.35.2\src/lib.rs C:\Users\benja\OneDrive\Documents\GitHub\Rust_Game\game\target\debug\build\sdl2-sys-480ea9db94c65671\out/sdl_bindings.rs
 
-C:\Users\jp3\OneDrive\Documents\GitHub\Rust_Game\game\target\debug\deps\sdl2_sys-9a5647e41400cad0.d: C:\Users\jp3\.cargo\registry\src\index.crates.io-6f17d22bba15001f\sdl2-sys-0.35.2\src/lib.rs C:\Users\jp3\OneDrive\Documents\GitHub\Rust_Game\game\target\debug\build\sdl2-sys-480ea9db94c65671\out/sdl_bindings.rs
+C:\Users\benja\OneDrive\Documents\GitHub\Rust_Game\game\target\debug\deps\sdl2_sys-9a5647e41400cad0.d: C:\Users\benja\.cargo\registry\src\index.crates.io-6f17d22bba15001f\sdl2-sys-0.35.2\src/lib.rs C:\Users\benja\OneDrive\Documents\GitHub\Rust_Game\game\target\debug\build\sdl2-sys-480ea9db94c65671\out/sdl_bindings.rs
 
-C:\Users\jp3\.cargo\registry\src\index.crates.io-6f17d22bba15001f\sdl2-sys-0.35.2\src/lib.rs:
-C:\Users\jp3\OneDrive\Documents\GitHub\Rust_Game\game\target\debug\build\sdl2-sys-480ea9db94c65671\out/sdl_bindings.rs:
+C:\Users\benja\.cargo\registry\src\index.crates.io-6f17d22bba15001f\sdl2-sys-0.35.2\src/lib.rs:
+C:\Users\benja\OneDrive\Documents\GitHub\Rust_Game\game\target\debug\build\sdl2-sys-480ea9db94c65671\out/sdl_bindings.rs:
 
-# env-dep:OUT_DIR=C:\\Users\\jp3\\OneDrive\\Documents\\GitHub\\Rust_Game\\game\\target\\debug\\build\\sdl2-sys-480ea9db94c65671\\out
+# env-dep:OUT_DIR=C:\\Users\\benja\\OneDrive\\Documents\\GitHub\\Rust_Game\\game\\target\\debug\\build\\sdl2-sys-480ea9db94c65671\\out
