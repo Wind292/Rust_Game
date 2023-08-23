2e7c610aade4703b98a3a5ab9f8ec006d75996cc
Wind292 <jp3@jayps.net>
2023-08-23 00:52:04 -0400

    FIXED wroks now
    added sdl.dll to same dir as exe

---
+++ game/target/debug/deps/bool32-c748e8971c7b0c1a.d
@@ -0,0 +1,7 @@
+C:\Users\jp3\OneDrive\Documents\GitHub\Rust_Game\game\target\debug\deps\bool32-c748e8971c7b0c1a.rmeta: C:\Users\jp3\.cargo\registry\src\index.crates.io-6f17d22bba15001f\bool32-0.1.1\src\lib.rs
+
+C:\Users\jp3\OneDrive\Documents\GitHub\Rust_Game\game\target\debug\deps\libbool32-c748e8971c7b0c1a.rlib: C:\Users\jp3\.cargo\registry\src\index.crates.io-6f17d22bba15001f\bool32-0.1.1\src\lib.rs
+
+C:\Users\jp3\OneDrive\Documents\GitHub\Rust_Game\game\target\debug\deps\bool32-c748e8971c7b0c1a.d: C:\Users\jp3\.cargo\registry\src\index.crates.io-6f17d22bba15001f\bool32-0.1.1\src\lib.rs
+
+C:\Users\jp3\.cargo\registry\src\index.crates.io-6f17d22bba15001f\bool32-0.1.1\src\lib.rs:
